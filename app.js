f540ae9efac4a46951000ca07c6c0dac1ec8c902
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-08-16 20:09:34 +1000

    MVC folders

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const userRouter = require("./routes/userRouter");
+
 app.get('/', (req, res) => {
     res.send('<H1>Tessagon</H1>')
 });
+
+app.use('/user', userRouter);
 
 app.listen(3000, () => {
     console.log('Tessagon is listening on port 3000!')
