48cc24dc8be01e17643f4916c11c819e23ba2de8
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-08-25 15:46:22 +1000

    log in, log out, sign up
    - sign in functionality not working
    - sign up working
    - no validation yet

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const bodyParser = require("body-parser");
 const app = express();
 
 // load view engine
@@ -11,8 +15,41 @@ app.set('view engine', 'pug');
 const db = require("./models");
 console.log(db);
 
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Passport Config
+require('./config/passport')(passport);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// global user object
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
 
 // GET home page
 app.get('/', (req, res) => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,23 @@
-
 const express = require("express");
 
+// create router
 const userRouter = express.Router();
 
 const userController = require("../controllers/userController");
 
-userRouter.get("/sign-up", userController.newUserForm);
+// Signing up
+userRouter.post("/signup", userController.addUser);
+
+// Sign Up form
+userRouter.get("/signup", userController.newUserForm);
+
+// log in form
+userRouter.get("/login", userController.logInPage);
+
+// logging in
+userRouter.post("/login", userController.logIn);
+
+// logging out
+userRouter.get("/logout", userController.logOutUser);
 
 module.exports = userRouter;
