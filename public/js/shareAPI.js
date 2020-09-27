7b096c216afcf52345964671ffeea6341eaaa350
JVALAYATH <61994150+JVALAYATH@users.noreply.github.com>
2020-09-28 00:25:34 +1000

    Delete shareAPI.js

--- public/js/shareAPI.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){ 
-    var x = document.URL;
-    document.getElementById("shareURL").src = 'https://www.facebook.com/plugins/share_button.php?href=' + x +'%2F&layout=button&size=small&width=67&height=20&appId';
- }) 
