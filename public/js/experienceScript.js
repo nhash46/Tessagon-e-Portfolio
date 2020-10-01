7dd92ea6e6658ce7de01500a4f1027144979ce19
Jonathan See <jtsee@student.unimelb.edu.au>
2020-10-01 16:30:16 +1000

    edit experience items without deletion

--- public/js/experienceScript.js
+++ public/js/experienceScript.js
@@ -3,10 +3,16 @@ $(document).ready(function(){
     $('.edit-experience').on('click', function(e){
         $target = $(e.target);
         const _id = $target.attr('experience-id');
+        var company = $("#company").prev.val();
+        var role = $("#role").prev.val();
+        var expStart = $("#start").prev.val();
+        var expEnd = $("#end").prev.val();
+        var dataString = 'company='+company+'&role='+role+'&experienceStartDate='+expStart+'&experienceEndDate='+expEnd;
         $.ajax({
             url: '/user/editExperience/'+_id,
             type: 'POST',
-            success: function(response){
+            data: dataString,
+            success: function(){
                 location.reload();
                 window.location.href='/user/profile#experience';
             },
@@ -25,7 +31,6 @@ $(document).ready(function(){
             url: '/user/experience/'+experience_id,
             type: 'DELETE',
             success: function(response){
-                alert('Deleting Experience');
                 location.reload();
                 window.location.href='/user/profile#experience';
             },
