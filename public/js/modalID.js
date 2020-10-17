e4c5599fa4adf3cdc5146665737253e5d1a782dd
Jonathan See <jtsee@student.unimelb.edu.au>
2020-10-17 20:58:48 +1100

    speed up fade out animation

--- public/js/modalID.js
+++ public/js/modalID.js
@@ -3,7 +3,3 @@ $('#experienceModal').on('show.bs.modal', function(e) {
     var experienceID = $(e.relatedTarget).data(exp-id);
     $(e.currentTarget).find('input[name="experienceID"]').val(experienceID);
 });
-
-$(document).ready(function() {
-    $('#alert').fadeOut(5000); // 5 seconds x 1000 milisec = 5000 milisec
-});
