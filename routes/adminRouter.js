d306abdc6f01d5b05a7077f78b4016825897af26
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-10-26 22:55:37 +1100

    Implemented: admin login

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,10 +3,29 @@ const mongoose = require("mongoose");
 const adminRouter = express.Router();
 const passport = require('passport');
 //const adminController = require("../controllers/adminController.js");
+const userController = require("../controllers/userController.js");
 
-adminRouter.get("/", (req, res) => {
+adminRouter.get("/", userController.authCheck, (req, res) => {
     res.render('admin');
 });
 
+adminRouter.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (user.isAdmin) {
+            req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                return res.redirect('/admin');
+            });
+        }
+        else {
+            req.session.message = {
+                type: 'danger',
+                intro: 'Oops,',
+                message: ' you do not have administrative rights.'
+            }
+            res.redirect('back'); 
+        }
+    })(req, res, next);
+});
 
 module.exports = adminRouter;
